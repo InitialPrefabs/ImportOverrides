e978e2968558e2a43d23de747886f93e92841a77
Porrith Suong <porrith.suong@gmail.com>
2024-08-16 08:27:39 -0400

    Added more settings for mipmap streaming.

--- Assets/com.initialprefabs.importoverrides/InitialPrefabs.ImportOverrides/GUIScope.cs
+++ Assets/com.initialprefabs.importoverrides/InitialPrefabs.ImportOverrides/GUIScope.cs
@@ -1,6 +1,21 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace InitialPrefabs.ImportOverrides {
+
+    public readonly ref struct IndentScope {
+
+        private readonly int indentLevel;
+
+        public IndentScope(int indentLevel) {
+            this.indentLevel = indentLevel;
+            EditorGUI.indentLevel += indentLevel;
+        }
+
+        public void Dispose() {
+            EditorGUI.indentLevel -= indentLevel;
+        }
+    }
 
     public readonly ref struct GUIScope {
 
--- Assets/com.initialprefabs.importoverrides/InitialPrefabs.ImportOverrides/TextureImporterSettingsPropertyDrawer.cs
+++ Assets/com.initialprefabs.importoverrides/InitialPrefabs.ImportOverrides/TextureImporterSettingsPropertyDrawer.cs
@@ -17,7 +17,104 @@ namespace InitialPrefabs.ImportOverrides {
 
         private static int AsMask(TextureImporterType type) => 1 << (int)type;
 
-        private static void HandleSprite(SerializedProperty root) {
+        private static bool Folded = false;
+
+        private static void HandleDefaultTexture(SerializedProperty root) {
+            var srgbProp = root.FindPropertyRelative(Variables.m_sRGBTexture);
+            var alphaSourceProp = root.FindPropertyRelative(Variables.m_AlphaSource);
+
+            EditorGUILayout.Space();
+
+            srgbProp.intValue = EditorGUILayout.Toggle(
+                new GUIContent("sRGB (Color Texture)"), srgbProp.intValue != 0) ?
+                1 : 0;
+
+            TextureImporterAlphaSource alphaSource = (TextureImporterAlphaSource)EditorGUILayout.EnumPopup(
+                new GUIContent("Alpha Source"),
+                (TextureImporterAlphaSource)alphaSourceProp.intValue);
+            alphaSourceProp.intValue = (int)alphaSource;
+
+            var alphaIsTransProp = root.FindPropertyRelative(Variables.m_AlphaIsTransparency);
+
+            switch (alphaSource) {
+                case TextureImporterAlphaSource.None:
+                    using (GUIScope.Disabled()) {
+                        alphaIsTransProp.intValue = EditorGUILayout.Toggle(
+                            new GUIContent("Alpha Is Transparency"),
+                            alphaIsTransProp.intValue != 0) ? 1 : 0;
+                    }
+                    break;
+                default:
+                    alphaIsTransProp.intValue = EditorGUILayout.Toggle(
+                        new GUIContent("Alpha Is Transparency"),
+                        alphaIsTransProp.intValue != 0) ? 1 : 0;
+                    break;
+            }
+
+            EditorGUILayout.Space();
+
+            // Draw the advanced dropdown
+            if (Folded = EditorGUILayout.Foldout(Folded, "Advance")) {
+                using (new IndentScope(1)) {
+                    var npotProp = root.FindPropertyRelative(Variables.m_NPOTScale);
+                    var npot = (TextureImporterNPOTScale)EditorGUILayout.EnumPopup(
+                        new GUIContent("Non-Power of 2", "How non power of twos are scaled on import."),
+                        (TextureImporterNPOTScale)npotProp.intValue);
+                    npotProp.intValue = (int)npot;
+
+                    var isReadableProp = root.FindPropertyRelative(Variables.m_IsReadable);
+                    isReadableProp.intValue = EditorGUILayout.Toggle(
+                        new GUIContent("Read/Write", "Enable access to raw pixel from code."),
+                        isReadableProp.intValue != 0) ? 1 : 0;
+
+                    var isVirtualTxtProp = root.FindPropertyRelative(Variables.m_VTOnly);
+                    isVirtualTxtProp.intValue = EditorGUILayout.Toggle(
+                        new GUIContent("Virtual Texture Only",
+                            "Texture is optimized for use as a virtual texture and can only be used as a Virtual Texture."),
+                        isVirtualTxtProp.intValue != 0) ? 1 : 0;
+
+                    var generateMipMapsProp = root.FindPropertyRelative(Variables.m_EnableMipMap);
+                    generateMipMapsProp.intValue = EditorGUILayout.Toggle(
+                        new GUIContent("Generate Mipmaps", "Create progressively smaller versions of the texture, " +
+                            "for reduced texture shimmering and better GPU performance when the texture is viewed at a distance."),
+                        generateMipMapsProp.intValue != 0) ? 1 : 0;
+
+                    if (generateMipMapsProp.intValue != 0) {
+                        using (new IndentScope(1)) {
+                            var ignoreMipMapLimitsProp = root.FindPropertyRelative(Variables.m_IgnoreMipmapLimit);
+                            bool useLimits = EditorGUILayout.Toggle(new GUIContent("Use Mipmap Limits",
+                                "Disable this if the number of mips to to upload should not be limited by the " +
+                                "quality settings. (effectively: always upload at full resolution, regardless of " +
+                                "the global mipmap limit or mipmap limit group."),
+                                ignoreMipMapLimitsProp.intValue == 0);
+                            ignoreMipMapLimitsProp.intValue = useLimits ? 0 : 1;
+                            // TODO: Limits are not shown here, figure out where they are stored
+
+                            var mipStreamingProp = root.FindPropertyRelative(Variables.m_StreamingMipmaps);
+                            var isStreaming = EditorGUILayout.Toggle(
+                                new GUIContent("Mip Steaming",
+                                "Only load larger mipmaps as needed to render the current game cameras. " +
+                                "Required texture streaming to be enabled in quality settings."),
+                                mipStreamingProp.intValue != 0);
+
+                            mipStreamingProp.intValue = isStreaming ? 1 : 0;
+                            if (isStreaming) {
+                                using var _ = new IndentScope(1);
+                                // Draw the priority for streaming
+                                var priorityProp = root.FindPropertyRelative(Variables.m_StreamingMipmapsPriority);
+                                priorityProp.intValue = Mathf.Clamp(EditorGUILayout.IntField(
+                                    new GUIContent("Priority", "Mipmap streaming priority when there's contention for " +
+                                    "resources. Positive numbers represent higher priority. Valid range is -128 to 127."),
+                                    priorityProp.intValue), -128, 127);
+                            }
+
+                            var mipMapModeProp = root.FindPropertyRelative(Variables.m_MipMapMode);
+                            mipMapModeProp.intValue = (int)(TextureImporterMipFilter)EditorGUILayout.EnumPopup(
+                                new GUIContent("MipmapFiltering"), (TextureImporterMipFilter)mipMapModeProp.intValue);
+                        }
+                    }
+                }
+            }
         }
 
         // TODO: For TextureImporterPlatformSettings use the BuildTargetGroup API
@@ -41,6 +138,9 @@ namespace InitialPrefabs.ImportOverrides {
             }
 
             switch (textureType) {
+                case TextureImporterType.Default:
+                    HandleDefaultTexture(property);
+                    break;
                 case TextureImporterType.GUI:
                 case TextureImporterType.Sprite:
                 case TextureImporterType.Cursor:
